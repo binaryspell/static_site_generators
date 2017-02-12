f259d9867bcf74616c0457d0a5fdf5260e52beee
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-02-12 08:52:21 +0530

    use base16 theme

--- hugo/init.sh
+++ hugo/init.sh
@@ -17,9 +17,11 @@ cd "$repo_path"
 # create a new post
 hugo new post/hello-world.md
 sed -i -e 's/draft = true/draft = false/g' content/post/hello-world.md
+echo -e "# Hello, World!\nWelcome to your doom!" >> content/post/hello-world.md
 
 # create theme
-git submodule add "https://github.com/vjeantet/hugo-theme-casper" themes/casper
-echo 'theme = "casper"' >> "config.toml"
+git submodule add "https://github.com/htdvisser/hugo-base16-theme" themes/base16
+echo 'theme = "base16"' >> "config.toml"
+sed -i -e '/baseURL/d' "config.toml"
 
 #docker run --rm --volume="$repo_path:/opt/build" "$docker_image" hugo new site "$repo_name" "$repo_path"""
