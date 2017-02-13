1d5bd7ffacf7b3a610a12a88084a2807800b7089
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-02-13 13:34:33 +0530

    save

--- hugo/init.sh
+++ hugo/init.sh
@@ -6,6 +6,8 @@ set -x
 echo "$*"
 
 repo_name="$1"
+repo_url="$2"
+site_url="$3"
 repo_path="/opt/build"
 
 cd "$repo_path"
@@ -26,4 +28,34 @@ echo -e "# Hello, World!\nWelcome to your doom!" >> content/post/hello-world.md
 # create theme
 git submodule add "https://github.com/htdvisser/hugo-base16-theme" themes/base16
 echo 'theme = "base16"' >> "config.toml"
-sed -i -e '/baseURL/d' "config.toml"
+sed -i -e '/baseURL/Id' "config.toml"
+echo "baseURL=\"$site_url\"" >> "config.toml"
+echo "relativeURLS = true" >> "config.toml"
+echo "canonifyurls = true" >> "config.toml"
+
+cat > ~/.gitconfig <<EOS
+[user]
+  email = binaryspell@cosmicvent.com
+  name = Binary Spell
+EOS
+
+# init commit
+git add .
+git commit -m init
+
+# build it
+hugo --verbose
+
+mv public /tmp/pub
+git checkout -b gh-pages
+rm -rf *
+mv /tmp/pub/* .
+
+git add .
+git commit -m "Built site"
+
+# push to github
+git remote add origin "$repo_url"
+git push origin master
+git push origin gh-pages
+
